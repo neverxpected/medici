ec4da4482dd43a326f4e5a9d9e67756c620973fa
neverxpected <ibiss55@gmail.com>
2026-04-07 02:37:34 -0500

    feat: initialize Tailwind CSS configuration and global styles with custom brand color palette

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        crimson: {
+          DEFAULT: '#8B0000',
+          light: '#A00000',
+        },
+        'off-white': '#F9F6F2',
+        'brand-black': '#0D0D0D',
+        'brand-white': '#FFFFFF',
+      },
+    },
+  },
+  plugins: [],
+}
+
